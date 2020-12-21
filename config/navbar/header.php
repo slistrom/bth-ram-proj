58940e5654722a09530eaf5affa81e88d9d5dfc3
Stefan <stefan@listrom.se>
2020-12-21 16:29:17 +0100

    first commit

---
+++ config/navbar/header.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Supply the basis for the navbar as an array.
+ */
+return [
+    // Use for styling the menu
+    "wrapper" => null,
+    "class" => "my-navbar rm-default rm-desktop",
+ 
+    // Here comes the menu items
+    "items" => [
+        [
+            "text" => "Hem",
+            "url" => "",
+            "title" => "Första sidan, börja här.",
+        ],
+        [
+            "text" => "Om",
+            "url" => "om",
+            "title" => "Om denna webbplats.",
+        ],
+        [
+            "text" => "Styleväljare",
+            "url" => "style",
+            "title" => "Välj stylesheet.",
+        ],
+        [
+            "text" => "Verktyg",
+            "url" => "verktyg",
+            "title" => "Verktyg och möjligheter för utveckling.",
+        ],
+    ],
+];
---
+++ content/index.md
@@ -0,0 +1,12 @@
+---
+title: "Front page"
+---
+One place for all your questions about crystals
+=========================
+
+This is the one place on the internet you can find all your answers about crystals.
+
+[FIGURE src="image/car.png?w=400" class="right" caption="Bild på en bil"]
+
+
+See you around!
