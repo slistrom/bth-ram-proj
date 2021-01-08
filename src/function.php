940af8b3429e033867f51c15f2041470c2602bd9
Stefan <stefan@listrom.se>
2021-01-08 12:48:58 +0100

    Included a general config file with help functions.

---
+++ src/function.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * Sanitize value for output in view.
+ *
+ * @param string $value to sanitize
+ *
+ * @return string beeing sanitized
+ */
+function esc($value)
+{
+    return htmlentities($value);
+}
+
+
+
+/**
+ * Function to create links for sorting.
+ *
+ * @param string $column the name of the database column to sort by
+ * @param string $route  prepend this to the anchor href
+ *
+ * @return string with links to order by column.
+ */
+function orderby($column, $route)
+{
+    return <<<EOD
+<span class="orderby">
+<a href="{$route}?column={$column}&order=asc">&darr;</a>
+<a href="{$route}?column={$column}&order=desc">&uarr;</a>
+</span>
+EOD;
+}
+
+
+
+/**
+ * Function to create links for sorting and keeping the original querystring.
+ *
+ * @param string $column the name of the database column to sort by
+ * @param string $route  prepend this to the anchor href
+ *
+ * @return string with links to order by column.
+ */
+function orderby2($column, $route)
+{
+    $asc = mergeQueryString(["orderby" => $column, "order" => "asc"], $route);
+    $desc = mergeQueryString(["orderby" => $column, "order" => "desc"], $route);
+    
+    return <<<EOD
+<span class="orderby">
+<a href="$asc">&darr;</a>
+<a href="$desc">&uarr;</a>
+</span>
+EOD;
+}
+
+
+
+/**
+ * Use current querystring as base, extract it to an array, merge it
+ * with incoming $options and recreate the querystring using the
+ * resulting array.
+ *
+ * @param array  $options to merge into exitins querystring
+ * @param string $prepend to the resulting query string
+ *
+ * @return string as an url with the updated query string.
+ */
+function mergeQueryString($options, $prepend = "?")
+{
+    // Parse querystring into array
+    $query = [];
+    parse_str($_SERVER["QUERY_STRING"], $query);
+
+    // Merge query string with new options
+    $query = array_merge($query, $options);
+
+    // Build and return the modified querystring as url
+    return $prepend . http_build_query($query);
+}
+
+
+/**
+ * Create a slug of a string, to be used as url.
+ *
+ * @param string $str the string to format as slug.
+ *
+ * @return str the formatted slug.
+ */
+function slugify($str)
+{
+    $str = mb_strtolower(trim($str));
+    $str = str_replace(['å','ä'], 'a', $str);
+    $str = str_replace('ö', 'o', $str);
+    $str = preg_replace('/[^a-z0-9-]/', '-', $str);
+    $str = trim(preg_replace('/-+/', '-', $str), '-');
+    return $str;
+}
+
+/**
+ * Get either a Gravatar URL or complete image tag for a specified email address.
+ *
+ * @param string $email The email address
+ * @param string $size Size in pixels, defaults to 80px [ 1 - 2048 ]
+ * @param string $default Default imageset to use [ 404 | mp | identicon | monsterid | wavatar ]
+ * @param string $rating Maximum rating (inclusive) [ g | pg | r | x ]
+ * @return String containing either just a URL or a complete image tag
+ * @source https://gravatar.com/site/implement/images/php/
+ */
+function getGravatar($email, $size = 80, $default = 'mp', $rating = 'g')
+{
+    $url = 'https://www.gravatar.com/avatar/';
+    $url .= md5(strtolower(trim($email)));
+    $url .= "?s=$size&d=$default&r=$rating";
+    return $url;
+}
